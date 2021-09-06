6ebe5651ad61feee544e0ad268c2d1bb3883293c
shruthiks123 <38246317+shruthiks123@users.noreply.github.com>
2021-09-06 17:23:34 +0800

    Update dockerbuild.sh

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -2,12 +2,3 @@
 yum update -y
 yum -y install docker ; /sbin/service docker start 
 docker build --tag example/node:latest --file ${PWD}/Dockerfile ${PWD} 
-
-========
-dockerBuild.sh
-
-
-
-docker build --tag praqma/native-scons:latest --file ${PWD}/Dockerfile ${PWD} 
-
-==========
